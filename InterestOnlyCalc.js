e7e3209f7c0f6689c8108225721cced02beb9060
Andy Faulkner <andrewfaulkner80@gmail.com>
2016-06-14 16:08:27 +0100

    Update

---
+++ InterestOnlyCalc.js
@@ -0,0 +1,19 @@
+//Create a click event to accept the values
+var event1 = document.getElementById("button1");
+    event1.addEventListener('click',calcInterest,false);
+
+//Start the main program logic
+function calcInterest(){
+    //Grab user inputs and assign to varibles
+    var amount = parseInt(document.getElementById("amount").value);
+    var interest = parseInt(document.getElementById("interest").value);
+    var term = parseInt(document.getElementById("term").value);
+    
+    //calculate interest on full amount
+    var totalInterest = (amount*(interest/100)) * term;
+    var interestPerMonth = totalInterest / (term *12);
+    interestPerMonth = interestPerMonth.toFixed(2);
+    //calculate the interest each month
+    
+document.getElementById("intValue").innerHTML = "The total interest will be £" + totalInterest + "<br>The interest payable each month will be £" + interestPerMonth;
+}
